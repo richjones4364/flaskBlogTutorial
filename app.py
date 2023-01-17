28ac7d35918a0da7fd221418d93de7b4d1ba0b2f
Rich Jones <103132290+richjones4364@users.noreply.github.com>
2023-01-17 12:17:55 +0000

    finally got the dictionary 'articles' displaying properly on the articles page

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+from article_data import Articles
 
 app = Flask(__name__)
 
+Articles = Articles()
+
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def index():
+    return render_template("index.html", title="Home")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+@app.route("/articles")
+def articles():
+    return render_template("articles.html", articles=Articles)
 
 
 if __name__ == "__main__":
---
+++ article_data.py
@@ -0,0 +1,19 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'First article',
+            'body': 'This is my first article!',
+            'author': 'Rich Jones',
+            'date_created': '17-01-2022',
+        },
+        {
+            'id': 2,
+            'title': 'Second article',
+            'body': 'This is my second article',
+            'author': 'Rich Jones',
+            'date_created': '17-01-2022',
+        },
+    ]
+
+    return articles
